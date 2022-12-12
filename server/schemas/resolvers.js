5f037fbc9cce8a4e9e62535aadbf579f159890d8
Volantis <putzstuck@gmail.com>
2022-12-12 09:29:18 -0500

    refactored and added code to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,76 @@
-const { Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    books: async () => {
-      return Book.find({});
+    users: async () => {
+      return User.find().populate('savedBooks');
     },
-    book: async (parent, { bookId }) => {
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('savedBooks');
+    },
+    savedBooks: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Book.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { bookId }) => {
       return Book.findOne({ _id: bookId });
     },
-  },
-  Mutation: {
-    saveBook: async (parent, { bookId }) => {
-      return Book.create({ bookId });
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
-    deleteBook: async (parent, { bookId }) => {
-      return Book.findOneAndDelete({ _id: bookId });
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const book = await Book.create({
+          bookId,
+          authors,
+          description,
+          title,
+          image,
+          link,
+        });
+        return book;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const book = await Book.findOneAndDelete({
+          _id: bookId,
+        });
+        return book;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 };
