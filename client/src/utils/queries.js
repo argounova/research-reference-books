a2037c93fd860565c7f445370c5869e9fca80634
Volantis <putzstuck@gmail.com>
2022-12-12 17:27:25 -0500

    copied in queries and mutations files...refactored queries.js file to match typedefs

---
+++ client/src/utils/queries.js
@@ -0,0 +1,63 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_BOOKS = gql`
+  query getBooks {
+    books {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
+
+export const QUERY_SINGLE_BOOK = gql`
+  query getSingleBook($bookId: String!) {
+    book(bookId: $bookId) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
