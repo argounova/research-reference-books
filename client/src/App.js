d8bda0fba8d553e4880d59d7efe47ea618b0300c
Volantis <putzstuck@gmail.com>
2022-12-12 18:14:32 -0500

    created apollo provider and added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,67 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<SearchBooks />}
+              />
+              <Route
+                path="/login"
+                element={<LoginForm />}
+              />
+              <Route
+                path="/signup"
+                element={<SignupForm />}
+              />
+              <Route  
+                path="/saved-books"
+                element={<SavedBooks />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
