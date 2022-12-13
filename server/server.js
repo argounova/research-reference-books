36a2948ee2419385dbcdd1d1ebbe08ac8b266145
Volantis <putzstuck@gmail.com>
2022-12-12 19:58:14 -0500

    reinstalled dependencies as most were not up to date or were missing

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -25,13 +34,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 startApolloServer(typeDefs, resolvers);
